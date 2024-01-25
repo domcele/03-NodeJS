922f7813fc0ab28fd527a1de1e2cb9c184360500
Dominykas Pavilionis <pavilions.dom@gmail.com>
2024-01-25 19:16:10 +0200

    renamed 2_class to 2_cao_assignment1 and create nre 2_class with express, cors, nodemon modules

---
+++ 2_class/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
